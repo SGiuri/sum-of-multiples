0ae37d4eda292e71adf52fbf0bf9632ef16d9821
SGiuri <s.giuri@issm.it>
2020-10-02 13:51:40 +0200

    Done

--- sum_of_multiples.py
+++ sum_of_multiples.py
@@ -1,2 +1,23 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    """
+
+    :param limit: a positive integer
+    :param multiples: a list containing integers
+    :return: the sum of multiplese
+
+    """
+
+    all_multiples = set()
+    if len(multiples) == 0:
+        return 0
+
+    for i in range(len(multiples)):
+        if multiples[i] != 0:
+            for m in range(multiples[i],limit,multiples[i]):
+                all_multiples.add(m)
+            else:
+                all_multiples.add(0)
+    whole_sum = sum(all_multiples)
+
+    return whole_sum
+
